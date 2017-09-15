a4904b07e49d09d2891f2e9274562622ce1e7c80
gingin77 <ginniehench@gmail.com>
2017-09-15 13:44:54 -0400

    minimize lines of code by calling methods that I've already written in the same file

--- classroom.rb
+++ classroom.rb
@@ -47,23 +47,7 @@ def letter_grade(score)
 end
 
 def final_letter_grades(grade_hash)
-  num_avg_hash = grade_hash.transform_values{|n| n.reduce(0,:+)/n.length}
-  letter_hash = num_avg_hash.transform_values do|l|
-    if l >= 90
-      "A"
-    elsif l < 90
-      case l
-      when 0...59.999
-        "F"
-      when 60...69.999
-        "D"
-      when 70...79.999
-        "C"
-      when 80...89.999
-        "B"
-      end
-    end
-  end
+  averages(grade_hash).transform_values {|v| letter_grade(v)}
 end
 
 def class_average(grade_hash)
@@ -72,7 +56,6 @@ def class_average(grade_hash)
 end
 
 def top_students(grade_hash, number_of_students)
-  num_avg_hash = grade_hash.transform_values{|n| n.sum/n.length}
-  num_avg_hash = num_avg_hash.sort_by {|rank| rank[1] }.reverse
-  num_avg_hash.take(number_of_students).collect{|students| students[0] }
+  sort_by_rank = averages(grade_hash).sort_by {|rank| rank[1] }.reverse
+  sort_by_rank.take(number_of_students).collect{|students| students[0] }
 end
