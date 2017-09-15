e2b9cc5f2b6f302565c210bedae795bfab7d5b0e
gingin77 <ginniehench@gmail.com>
2017-09-15 10:41:59 -0400

    removed all comments

--- classroom.rb
+++ classroom.rb
@@ -1,26 +1,14 @@
 require 'pry'
-# Given a grade_hash, student name, and assignment number, return the score
-# for that student and assignment. Note that Ruby counts arrays from 0, but
-# we are referring to them as 1-10.
 
 def assignment_score(grade_hash, student, assignment_num)
   grade_hash[student][assignment_num-1]
 end
-
-
-# Given a grade_hash and assignment number, return all scores for that
-# assignment. Note that Ruby counts arrays from 0, but we are referring to
-# them as 1-10.
 
 def assignment_scores(grade_hash, assignment_num)
   just_grades = grade_hash.values.to_a
   select_assignments = just_grades.map {|grade| grade[assignment_num-1] }
   select_assignments
 end
-
-# Given a grade_hash and assignment number, return the average score for that
-# assignment. Note that Ruby counts arrays from 0, but we are referring to
-# them as 1-10.
 
 def assignment_average_score(grade_hash, assignment_num)
   just_grades = grade_hash.values.to_a
@@ -29,10 +17,6 @@ def assignment_average_score(grade_hash, assignment_num)
   average = sum/select_assignments.length
   average
 end
-
-# Return a hash of students and their average score.
-# TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
-# use .to_h. Also look at Hash#transform_values.
 
 def averages(grade_hash)
   new_hash = {}
@@ -45,12 +29,6 @@ def averages(grade_hash)
   new_hash
 end
 
-# Return a letter grade for a numerical score.
-# 90+ => A
-# 80-89 => B
-# 70-79 => C
-# 60-69 => D
-# < 60 => F
 def letter_grade(score)
   if score >= 90
     "A"
@@ -68,8 +46,6 @@ def letter_grade(score)
   end
 end
 
-# Return a hash of students and their final letter grade, as determined
-# by their average.
 def final_letter_grades(grade_hash)
   num_avg_hash = grade_hash.transform_values{|n| n.reduce(0,:+)/n.length}
   letter_hash = num_avg_hash.transform_values do|l|
@@ -89,7 +65,6 @@ def final_letter_grades(grade_hash)
     end
   end
 end
-
 
 def class_average(grade_hash)
   total = grade_hash.values.flatten
